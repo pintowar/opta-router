7d22f13acb2722a990502a5a6d5defefd57e13d8
Thiago Oliveira <pintowar@gmail.com>
2023-05-01 10:36:59 -0300

    client lint

--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -34,7 +34,6 @@ onBeforeMount(async () => {
                 height="26"
                 width="26"
                 version="1.1"
-                id="_x32_"
                 xmlns="http://www.w3.org/2000/svg"
                 xmlns:xlink="http://www.w3.org/1999/xlink"
                 viewBox="0 0 512 512"
