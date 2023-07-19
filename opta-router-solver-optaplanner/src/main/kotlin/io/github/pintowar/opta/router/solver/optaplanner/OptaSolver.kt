893110d02b22043b4f461a8ab9cd0dd61e472a53
Thiago Oliveira <pintowar@gmail.com>
2023-07-19 00:17:48 -0300

    updating optaplanner solver to new solver interface

--- opta-router-solver-optaplanner/src/main/kotlin/io/github/pintowar/opta/router/solver/optaplanner/OptaSolver.kt
+++ opta-router-solver-optaplanner/src/main/kotlin/io/github/pintowar/opta/router/solver/optaplanner/OptaSolver.kt
@@ -1,43 +1,45 @@
 package io.github.pintowar.opta.router.solver.optaplanner
 
-import io.github.pintowar.opta.router.core.domain.models.SolverStatus
 import io.github.pintowar.opta.router.core.domain.models.VrpSolution
-import io.github.pintowar.opta.router.core.domain.models.VrpSolutionRequest
 import io.github.pintowar.opta.router.core.domain.models.matrix.Matrix
+import io.github.pintowar.opta.router.core.solver.SolutionFlow
 import io.github.pintowar.opta.router.core.solver.SolverConfig
 import io.github.pintowar.opta.router.core.solver.spi.Solver
+import kotlinx.coroutines.channels.trySendBlocking
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.suspendCancellableCoroutine
 import org.optaplanner.core.api.solver.SolverFactory
 import org.optaplanner.core.config.solver.termination.TerminationConfig
 import org.optaplanner.examples.vehiclerouting.domain.VehicleRoutingSolution
-import java.util.UUID
+import kotlin.coroutines.resume
 import org.optaplanner.core.config.solver.SolverConfig as SC
 
-class OptaSolver(key: UUID, name: String, config: SolverConfig) : Solver(key, name, config) {
+class OptaSolver : Solver {
     private val configPath = "org/optaplanner/examples/vehiclerouting/vehicleRoutingSolverConfig.xml"
-    private val solverConfig = SC.createFromXmlResource(configPath).apply {
-        terminationConfig = TerminationConfig().apply {
-            overwriteSpentLimit(config.timeLimit)
+    private val solverConfig = SC.createFromXmlResource(configPath)
+
+    override val name: String = "optaplanner"
+
+    override fun solutionFlow(initialSolution: VrpSolution, matrix: Matrix, config: SolverConfig): Flow<SolutionFlow> {
+        val solver = SolverFactory.create<VehicleRoutingSolution>(solverConfig.apply {
+            terminationConfig = TerminationConfig().apply {
+                overwriteSpentLimit(config.timeLimit)
+            }
+        }).buildSolver()
+
+        return callbackFlow {
+            solver.addEventListener { evt ->
+                val sol = evt.newBestSolution
+                trySendBlocking(SolutionFlow(sol.toDTO(initialSolution.problem, matrix)))
+            }
+
+            val sol = suspendCancellableCoroutine<VehicleRoutingSolution> { continuation ->
+                continuation.invokeOnCancellation { solver.terminateEarly() }
+                continuation.resume(solver.solve(initialSolution.toSolverSolution(matrix)))
+            }
+            send(SolutionFlow(sol.toDTO(initialSolution.problem, matrix), true))
+            close()
         }
     }
-    private val solverFactory = SolverFactory.create<VehicleRoutingSolution>(solverConfig)
-    private val solver = solverFactory.buildSolver()
-
-    override fun solve(initialSolution: VrpSolution, matrix: Matrix, callback: (VrpSolutionRequest) -> Unit) {
-        val problem = initialSolution.problem
-
-        solver.addEventListener { evt ->
-            val sol = evt.newBestSolution
-            callback(VrpSolutionRequest(sol.toDTO(problem, matrix), SolverStatus.RUNNING, key))
-        }
-
-        if (!initialSolution.isEmpty()) callback(VrpSolutionRequest(initialSolution, SolverStatus.RUNNING, key))
-        val sol = solver.solve(initialSolution.toSolverSolution(matrix))
-        callback(VrpSolutionRequest(sol.toDTO(problem, matrix), SolverStatus.TERMINATED, key))
-    }
-
-    override fun terminate() {
-        solver.terminateEarly()
-    }
-
-    override fun isSolving() = solver.isSolving
 }
