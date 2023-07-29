0bfb561d74e97da3c27f115c6d33c5d61e679713
Thiago Oliveira <pintowar@gmail.com>
2023-07-29 15:40:30 -0300

    general refactoring (on termination process)

--- opta-router-core/src/main/kotlin/io/github/pintowar/opta/router/core/solver/VrpSolverManager.kt
+++ opta-router-core/src/main/kotlin/io/github/pintowar/opta/router/core/solver/VrpSolverManager.kt
@@ -1,8 +1,9 @@
 package io.github.pintowar.opta.router.core.solver
 
 import io.github.pintowar.opta.router.core.domain.models.SolverStatus
+import io.github.pintowar.opta.router.core.domain.models.VrpSolution
 import io.github.pintowar.opta.router.core.domain.models.VrpSolutionRequest
-import io.github.pintowar.opta.router.core.domain.ports.BroadcastPort
+import io.github.pintowar.opta.router.core.domain.models.matrix.Matrix
 import io.github.pintowar.opta.router.core.domain.ports.SolverEventsPort
 import io.github.pintowar.opta.router.core.domain.repository.SolverRepository
 import io.github.pintowar.opta.router.core.solver.spi.Solver
@@ -10,38 +11,33 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import mu.KotlinLogging
 import java.time.Duration
 import java.util.UUID
+import java.util.concurrent.CancellationException
 import java.util.concurrent.ConcurrentHashMap
 
 private val logger = KotlinLogging.logger {}
 
-/**
- * This service is responsible to solve VRP problems asynchronously, update the application status, solver and solutions
- * storages. It also sends notifications to the WebSocket queue of the proper user (just for the one that executed the
- * application).
- *
- */
 class VrpSolverManager(
     private val timeLimit: Duration,
     private val solverEvents: SolverEventsPort,
     private val solverRepository: SolverRepository,
 ) {
 
+    private class UserCancellationException(val clear: Boolean) : CancellationException("User cancellation command.")
+
     private val supervisorJob = SupervisorJob()
     private val managerScope = CoroutineScope(supervisorJob + Dispatchers.Default)
     private val solverKeys = ConcurrentHashMap<UUID, Job>()
 
     init {
-        solverEvents.addRequestSolverListener { solve(it.problemId, it.solverKey, it.solverName) }
+        solverEvents.addRequestSolverListener { fetchAndSolve(it.problemId, it.solverKey, it.solverName) }
         solverEvents.addBroadcastCancelListener { cancelSolver(it.solverKey, it.clear) }
     }
 
@@ -53,13 +49,18 @@ class VrpSolverManager(
         solverEvents.enqueueSolutionRequest(SolverEventsPort.SolutionRequestCommand(solutionRequest, clear))
     }
 
-    private fun solve(problemId: Long, uuid: UUID, solverName: String) {
+    private fun fetchAndSolve(problemId: Long, uuid: UUID, solverName: String) {
         if (solverKeys.containsKey(uuid)) return
-
         val currentMatrix = solverRepository.currentMatrix(problemId) ?: return
         val solutionRequest = solverRepository.currentSolutionRequest(problemId) ?: return
         val solverKey = solutionRequest.solverKey ?: return
         val currentSolution = solutionRequest.solution
+
+        solve(solverKey, currentSolution, currentMatrix, solverName)
+    }
+
+    private fun solve(solverKey: UUID, currentSolution: VrpSolution, currentMatrix: Matrix, solverName: String) {
+        if (solverKeys.containsKey(solverKey)) return
 
         solverKeys[solverKey] = managerScope.launch {
             val scope = this
@@ -72,26 +73,17 @@ class VrpSolverManager(
                     logger.debug { "onEach (${scope.isActive}): $solverKey | ${bestSolution.getTotalDistance()}" }
                     enqueueSolution(VrpSolutionRequest(it, SolverStatus.RUNNING, solverKey))
                 }
-                .onCompletion {
+                .onCompletion { ex ->
                     logger.debug { "onEnd (${scope.isActive}): $solverKey | ${bestSolution.getTotalDistance()}" }
-                    enqueueSolution(VrpSolutionRequest(bestSolution, SolverStatus.TERMINATED, solverKey))
+                    val solRequest = VrpSolutionRequest(bestSolution, SolverStatus.TERMINATED, solverKey)
+                    val shouldClear = ex is UserCancellationException && ex.clear
+                    enqueueSolution(solRequest, shouldClear)
                 }
                 .collect()
         }
     }
 
     private fun cancelSolver(uuid: UUID, clear: Boolean) {
-        solverRepository.currentSolverRequest(uuid)?.also { solverRequest ->
-            if (solverRequest.status != SolverStatus.NOT_SOLVED) {
-                runBlocking {
-                    solverKeys.remove(uuid)?.cancelAndJoin()
-                }
-                if (clear) {
-                    solverRepository.currentSolutionRequest(solverRequest.problemId)?.let {
-                        enqueueSolution(it, true)
-                    }
-                }
-            }
-        }
+        solverKeys.remove(uuid)?.cancel(UserCancellationException(clear))
     }
 }
--- opta-router-core/src/main/kotlin/io/github/pintowar/opta/router/core/solver/VrpSolverService.kt
+++ opta-router-core/src/main/kotlin/io/github/pintowar/opta/router/core/solver/VrpSolverService.kt
@@ -14,21 +14,17 @@ import java.time.Duration
 import java.util.*
 import java.util.concurrent.ConcurrentHashMap
 
-/**
- * This service is responsible to solve VRP problems asynchronously, update the application status, solver and solutions
- * storages. It also sends notifications to the WebSocket queue of the proper user (just for the one that executed the
- * application).
- *
- */
 class VrpSolverService(
-        private val solverEvents: SolverEventsPort,
-        private val solverRepository: SolverRepository,
-        private val broadcastPort: BroadcastPort
+    private val solverEvents: SolverEventsPort,
+    private val solverRepository: SolverRepository,
+    private val broadcastPort: BroadcastPort
 ) {
 
     init {
         solverEvents.addSolutionRequestListener { updateAndBroadcast(it.solutionRequest, it.clear) }
     }
+
+    fun solverNames() = Solver.getNamedSolvers().keys
 
     fun currentSolutionRequest(problemId: Long): VrpSolutionRequest? {
         return solverRepository.currentSolutionRequest(problemId)
@@ -45,25 +41,32 @@ class VrpSolverService(
     fun enqueueSolverRequest(problemId: Long, solverName: String): UUID? {
         return solverRepository.enqueue(problemId, solverName)?.let { request ->
             solverEvents.enqueueRequestSolver(
-                    SolverEventsPort.RequestSolverCommand(
-                            request.problemId,
-                            request.requestKey,
-                            solverName
-                    )
+                SolverEventsPort.RequestSolverCommand(
+                    request.problemId,
+                    request.requestKey,
+                    solverName
+                )
             )
             request.requestKey
         }
     }
 
-    fun terminate(solverKey: UUID) {
-        solverEvents.broadcastCancelSolver(SolverEventsPort.CancelSolverCommand(solverKey))
-    }
+    fun terminate(solverKey: UUID): Unit = terminateEarly(solverKey, false)
 
-    fun clear(solverKey: UUID) {
-        solverEvents.broadcastCancelSolver(SolverEventsPort.CancelSolverCommand(solverKey, true))
-    }
+    fun clear(solverKey: UUID): Unit = terminateEarly(solverKey, true)
 
-    fun solverNames() = Solver.getNamedSolvers().keys
+    private fun terminateEarly(solverKey: UUID, clear: Boolean) {
+        solverRepository.currentSolverRequest(solverKey)?.also { solverRequest ->
+            // TODO: must act when is ENQUEUED
+            if (solverRequest.status == SolverStatus.RUNNING) {
+                solverEvents.broadcastCancelSolver(SolverEventsPort.CancelSolverCommand(solverKey, clear))
+            } else if (solverRequest.status == SolverStatus.TERMINATED && clear) {
+                solverRepository.currentSolutionRequest(solverRequest.problemId)?.let { solutionRequest ->
+                    solverEvents.enqueueSolutionRequest(SolverEventsPort.SolutionRequestCommand(solutionRequest, true))
+                }
+            }
+        }
+    }
 
     private fun updateAndBroadcast(solRequest: VrpSolutionRequest, clear: Boolean) {
         val newSolRequest = solverRepository
