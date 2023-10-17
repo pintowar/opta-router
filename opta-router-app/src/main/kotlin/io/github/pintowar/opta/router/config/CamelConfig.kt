c8f8c42e487ed54a290ad0fd1fb3a3879da308e3
Thiago Oliveira <pintowar@gmail.com>
2023-10-17 00:05:01 -0300

    adjusting/simplyfing camel routes

--- opta-router-app/src/main/kotlin/io/github/pintowar/opta/router/config/CamelConfig.kt
+++ opta-router-app/src/main/kotlin/io/github/pintowar/opta/router/config/CamelConfig.kt
@@ -3,27 +3,36 @@ package io.github.pintowar.opta.router.config
 import io.github.pintowar.opta.router.core.domain.ports.BroadcastPort
 import io.github.pintowar.opta.router.core.domain.ports.SolverEventsPort
 import org.apache.camel.ProducerTemplate
+import org.apache.camel.component.hazelcast.HazelcastConstants
+import org.apache.camel.component.hazelcast.HazelcastOperation
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 @Component
 class CamelConfig(
+    @Value("\${camel.route.producer.request-solver}") private val requestSolver: String,
+    @Value("\${camel.route.producer.solution-request}") private val solutionRequest: String,
+    @Value("\${camel.route.producer.solution-topic}") private val solutionTopic: String,
+    @Value("\${camel.route.producer.cancel-solver-topic}") private val cancelSolverTopic: String,
     private val template: ProducerTemplate,
 ) : SolverEventsPort, BroadcastPort {
 
+    private val hazelHeaders = mapOf(HazelcastConstants.OPERATION to HazelcastOperation.PUT)
+
     override fun enqueueRequestSolver(command: SolverEventsPort.RequestSolverCommand) {
-        template.sendBody("direct:request-solver-direct", command)
+        template.sendBodyAndHeaders(requestSolver, command, hazelHeaders)
     }
 
     override fun enqueueSolutionRequest(command: SolverEventsPort.SolutionRequestCommand) {
-        template.sendBody("direct:solution-request-queue", command)
+        template.sendBodyAndHeaders(solutionRequest, command, hazelHeaders)
     }
 
     override fun broadcastSolution(command: BroadcastPort.SolutionCommand) {
-        template.sendBody("direct:solution-topic", command)
+        template.sendBody(solutionTopic, command)
     }
 
     override fun broadcastCancelSolver(command: SolverEventsPort.CancelSolverCommand) {
-        template.sendBody("direct:cancel-solver-topic", command)
+        template.sendBody(cancelSolverTopic, command)
     }
 
 }
--- opta-router-app/src/main/kotlin/io/github/pintowar/opta/router/config/camel/CamelEventsRegistry.kt
+++ opta-router-app/src/main/kotlin/io/github/pintowar/opta/router/config/camel/CamelEventsRegistry.kt
@@ -1,54 +1,39 @@
 package io.github.pintowar.opta.router.config.camel
 
-import com.hazelcast.core.HazelcastInstance
 import io.github.pintowar.opta.router.adapters.handler.WebSocketHandler
 import io.github.pintowar.opta.router.core.solver.VrpSolverManager
 import io.github.pintowar.opta.router.core.solver.VrpSolverService
 import org.apache.camel.builder.RouteBuilder
-import org.apache.camel.builder.endpoint.StaticEndpointBuilders.*
-import org.apache.camel.component.hazelcast.HazelcastConstants
-import org.apache.camel.component.hazelcast.HazelcastOperation
-import org.apache.camel.component.hazelcast.queue.HazelcastQueueConsumerMode
 import org.springframework.stereotype.Component
 
 @Component
-class CamelEventsRegistry(hz: HazelcastInstance) : RouteBuilder() {
-
-    private val requestSolverQueue = hazelcastQueue("request-solver-queue").hazelcastInstanceName(hz.name)
-    private val solutionRequestQueue = hazelcastQueue("solution-request-queue").hazelcastInstanceName(hz.name)
-
-    private val solutionTopic = hazelcastTopic("solution-topic").hazelcastInstanceName(hz.name)
-    private val cancelTopic = hazelcastTopic("cancel-solver-topic").hazelcastInstanceName(hz.name).reliable(true)
+class CamelEventsRegistry : RouteBuilder() {
 
     override fun configure() {
-        from(direct("request-solver-direct"))
-            .setHeader(HazelcastConstants.OPERATION, constant(HazelcastOperation.PUT))
-            .to(requestSolverQueue)
 
-        from(requestSolverQueue.queueConsumerMode(HazelcastQueueConsumerMode.POLL))
-            .bean(VrpSolverManager::class.java, "solve(\${body.solverKey}, \${body.detailedSolution}, \${body.solverName})")
+        from("{{camel.route.consumer.request-solver}}")
+            .routeId("request.solver.queue")
+            .bean(
+                VrpSolverManager::class.java,
+                "solve(\${body.solverKey}, \${body.detailedSolution}, \${body.solverName})"
+            )
 
-        from(direct("solution-request-queue"))
-            .setHeader(HazelcastConstants.OPERATION, constant(HazelcastOperation.PUT))
-            .to(solutionRequestQueue)
-
-        from(solutionRequestQueue.queueConsumerMode(HazelcastQueueConsumerMode.POLL))
+        from("{{camel.route.consumer.solution-request}}")
+            .routeId("solution.request.queue")
             .bean(VrpSolverService::class.java, "updateAndBroadcast(\${body.solutionRequest}, \${body.clear})")
 
-        from(direct("solution-topic"))
-            .to(solutionTopic)
-
-        from(solutionTopic)
+        from("{{camel.route.consumer.solution-topic}}")
+            .routeId("solution.topic")
             .transform().spel("#{body.messageObject}")
             .bean(WebSocketHandler::class.java, "broadcast(\${body.solutionRequest})")
 
-        from(direct("cancel-solver-topic"))
-            .to(cancelTopic)
-
-        from(cancelTopic)
+        from("{{camel.route.consumer.cancel-solver-topic}}")
+            .routeId("cancel.solver.topic")
             .transform().spel("#{body.messageObject}")
-            .bean(VrpSolverManager::class.java, "cancelSolver(\${body.solverKey}, \${body.currentStatus}, \${body.clear})")
-
+            .bean(
+                VrpSolverManager::class.java,
+                "cancelSolver(\${body.solverKey}, \${body.currentStatus}, \${body.clear})"
+            )
     }
 
 }
