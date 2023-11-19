b98e3d9f3ccfb0469b01ee515ee69ae70112407c
Thiago Oliveira <pintowar@gmail.com>
2023-11-19 12:38:01 -0300

    adding openapi docs to function router

--- opta-router-app/src/main/kotlin/io/github/pintowar/opta/router/config/RestHandlerConfig.kt
+++ opta-router-app/src/main/kotlin/io/github/pintowar/opta/router/config/RestHandlerConfig.kt
@@ -1,12 +1,20 @@
 package io.github.pintowar.opta.router.config
 
-import io.github.pintowar.opta.router.core.domain.models.SolverPanel
-import io.github.pintowar.opta.router.core.domain.models.VrpSolutionRequest
+import io.github.pintowar.opta.router.core.domain.models.*
 import io.github.pintowar.opta.router.core.domain.ports.GeoPort
 import io.github.pintowar.opta.router.core.domain.ports.VrpProblemPort
 import io.github.pintowar.opta.router.core.domain.ports.VrpSolverRequestPort
 import io.github.pintowar.opta.router.core.domain.ports.VrpSolverSolutionPort
 import io.github.pintowar.opta.router.core.solver.VrpSolverService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.enums.ParameterIn
+import io.swagger.v3.oas.annotations.media.ArraySchema
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import org.springdoc.core.annotations.RouterOperation
+import org.springdoc.core.annotations.RouterOperations
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
@@ -19,6 +27,144 @@ class RestHandlerConfig {
     data class PanelSolutionState(val solverPanel: SolverPanel, val solutionState: VrpSolutionRequest)
 
     @Bean
+    @RouterOperations(
+        RouterOperation(
+            path = "/api/solver/solver-names",
+            operation = Operation(
+                operationId = "solverNames",
+                method = "GET",
+                tags = ["solver-handler"],
+                responses = [
+                    ApiResponse(
+                        responseCode = "200",
+                        description = "OK",
+                        content = [Content(array = ArraySchema(schema = Schema(implementation = String::class)))]
+                    )
+                ]
+            )
+        ),
+        RouterOperation(
+            path = "/api/solver/{id}/solve/{solverName}",
+            operation = Operation(
+                operationId = "solve",
+                method = "POST",
+                tags = ["solver-handler"],
+                parameters = [
+                    Parameter(
+                        `in` = ParameterIn.PATH,
+                        name = "id",
+                        required = true,
+                        schema = Schema(type = "integer")
+                    )
+                ],
+                responses = [
+                    ApiResponse(
+                        responseCode = "200",
+                        description = "OK",
+                        content = [Content(schema = Schema(implementation = SolverStatus::class))]
+                    )
+                ]
+            )
+        ),
+        RouterOperation(
+            path = "/api/solver/{id}/terminate",
+            operation = Operation(
+                operationId = "terminate",
+                method = "POST",
+                tags = ["solver-handler"],
+                parameters = [
+                    Parameter(
+                        `in` = ParameterIn.PATH,
+                        name = "id",
+                        required = true,
+                        schema = Schema(type = "integer")
+                    )
+                ],
+                responses = [
+                    ApiResponse(
+                        responseCode = "200",
+                        description = "OK",
+                        content = [Content(schema = Schema(implementation = SolverStatus::class))]
+                    )
+                ]
+            )
+        ),
+        RouterOperation(
+            path = "/api/solver/{id}/clean",
+            operation = Operation(
+                operationId = "clear",
+                method = "POST",
+                tags = ["solver-handler"],
+                parameters = [
+                    Parameter(
+                        `in` = ParameterIn.PATH,
+                        name = "id",
+                        required = true,
+                        schema = Schema(type = "integer")
+                    )
+                ],
+                responses = [
+                    ApiResponse(
+                        responseCode = "200",
+                        description = "OK",
+                        content = [Content(schema = Schema(implementation = SolverStatus::class))]
+                    )
+                ]
+            )
+        ),
+        RouterOperation(
+            path = "/api/solver/{id}/detailed-path/{isDetailed}",
+            operation = Operation(
+                operationId = "detailedPath",
+                method = "PUT",
+                tags = ["solver-handler"],
+                parameters = [
+                    Parameter(
+                        `in` = ParameterIn.PATH,
+                        name = "id",
+                        required = true,
+                        schema = Schema(type = "integer")
+                    ),
+                    Parameter(
+                        `in` = ParameterIn.PATH,
+                        name = "isDetailed",
+                        required = true,
+                        schema = Schema(type = "boolean")
+                    )
+                ],
+                responses = [
+                    ApiResponse(
+                        responseCode = "200",
+                        description = "OK",
+                        content = [Content(schema = Schema(implementation = SolverStatus::class))]
+                    )
+                ]
+            )
+        ),
+        RouterOperation(
+            path = "/api/solver/{id}/solution-panel",
+            operation = Operation(
+                operationId = "solutionState",
+                method = "GET",
+                tags = ["solver-handler"],
+                parameters = [
+                    Parameter(
+                        `in` = ParameterIn.PATH,
+                        name = "id",
+                        required = true,
+                        schema = Schema(type = "integer")
+                    )
+                ],
+                responses = [
+                    ApiResponse(
+                        responseCode = "200",
+                        description = "OK",
+                        content = [Content(schema = Schema(implementation = PanelSolutionState::class))]
+                    )
+                ]
+            )
+        ),
+    )
     fun solverRouter(
         solverService: VrpSolverService,
         geoService: GeoPort,
@@ -71,6 +217,46 @@ class RestHandlerConfig {
     }
 
     @Bean
+    @RouterOperations(
+        RouterOperation(
+            path = "/api/vrp-problems",
+            operation = Operation(
+                operationId = "index",
+                method = "GET",
+                tags = ["vrp-problem-handler"],
+                responses = [
+                    ApiResponse(
+                        responseCode = "200",
+                        description = "OK",
+                        content = [Content(array = ArraySchema(schema = Schema(implementation = VrpProblem::class)))]
+                    )
+                ]
+            )
+        ),
+        RouterOperation(
+            path = "/api/vrp-problems/{id}",
+            operation = Operation(
+                operationId = "show",
+                method = "GET",
+                tags = ["vrp-problem-handler"],
+                parameters = [
+                    Parameter(
+                        `in` = ParameterIn.PATH,
+                        name = "id",
+                        required = true,
+                        schema = Schema(type = "integer")
+                    )
+                ],
+                responses = [
+                    ApiResponse(
+                        responseCode = "200",
+                        description = "OK",
+                        content = [Content(schema = Schema(implementation = VrpProblem::class))]
+                    )
+                ]
+            )
+        )
+    )
     fun problemRouter(repo: VrpProblemPort) = coRouter {
         "/api/vrp-problems".nest {
             GET("") { ok().bodyAndAwait(repo.listAll()) }
@@ -84,6 +270,60 @@ class RestHandlerConfig {
     }
 
     @Bean
+    @RouterOperations(
+        RouterOperation(
+            path = "/api/solver-history/{problemId}/requests/{solverName}",
+            operation = Operation(
+                operationId = "requests",
+                method = "GET",
+                tags = ["solver-history-handler"],
+                parameters = [
+                    Parameter(
+                        `in` = ParameterIn.PATH,
+                        name = "problemId",
+                        required = true,
+                        schema = Schema(type = "integer")
+                    ),
+                    Parameter(
+                        `in` = ParameterIn.PATH,
+                        name = "solverName",
+                        required = true,
+                        schema = Schema(type = "string")
+                    )
+                ],
+                responses = [
+                    ApiResponse(
+                        responseCode = "200",
+                        description = "OK",
+                        content = [Content(array = ArraySchema(schema = Schema(implementation = VrpSolverRequest::class)))]
+                    )
+                ]
+            )
+        ),
+        RouterOperation(
+            path = "/api/solver-history/{problemId}/solutions",
+            operation = Operation(
+                operationId = "solutions",
+                method = "GET",
+                tags = ["solver-history-handler"],
+                parameters = [
+                    Parameter(
+                        `in` = ParameterIn.PATH,
+                        name = "problemId",
+                        required = true,
+                        schema = Schema(type = "integer")
+                    )
+                ],
+                responses = [
+                    ApiResponse(
+                        responseCode = "200",
+                        description = "OK",
+                        content = [Content(array = ArraySchema(schema = Schema(implementation = VrpSolverObjective::class)))]
+                    )
+                ]
+            )
+        )
+    )
     fun solverHistoryRouter(
         vrpSolverRequestPort: VrpSolverRequestPort,
         vrpSolverSolutionPort: VrpSolverSolutionPort
