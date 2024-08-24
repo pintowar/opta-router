1e8aba34fffcc5b8cde6c94f13f9f6697400f530
Thiago Oliveira <pintowar@gmail.com>
2024-08-23 22:32:17 -0300

    updating database config

--- opta-router-app/src/main/kotlin/io/github/pintowar/opta/router/config/rest/DatabaseConfig.kt
+++ opta-router-app/src/main/kotlin/io/github/pintowar/opta/router/config/rest/DatabaseConfig.kt
@@ -6,25 +6,28 @@ import org.flywaydb.core.Flyway
 import org.jooq.DSLContext
 import org.jooq.ExecuteListenerProvider
 import org.jooq.SQLDialect
+import org.jooq.TransactionProvider
 import org.jooq.conf.Settings
 import org.jooq.impl.DSL
 import org.jooq.impl.DefaultExecuteListenerProvider
 import org.springframework.beans.factory.ObjectProvider
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.autoconfigure.flyway.FlywayProperties
-import org.springframework.boot.autoconfigure.jooq.JooqExceptionTranslator
+import org.springframework.boot.autoconfigure.jooq.ExceptionTranslatorExecuteListener
+import org.springframework.boot.autoconfigure.jooq.SpringTransactionProvider
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.core.annotation.Order
-import org.springframework.data.r2dbc.config.EnableR2dbcAuditing
 import org.springframework.r2dbc.connection.TransactionAwareConnectionFactoryProxy
+import org.springframework.transaction.PlatformTransactionManager
 
 @Configuration
 @Profile(ConfigData.REST_PROFILE)
-@EnableR2dbcAuditing
 @EnableConfigurationProperties(FlywayProperties::class)
 class DatabaseConfig(
     @Value("\${spring.jooq.sql-dialect}") private val sqlDialect: SQLDialect,
@@ -33,9 +36,26 @@ class DatabaseConfig(
 ) {
 
     @Bean
+    @ConditionalOnBean(PlatformTransactionManager::class)
+    @ConditionalOnMissingBean(
+        TransactionProvider::class
+    )
+    fun transactionProvider(txManager: PlatformTransactionManager): SpringTransactionProvider {
+        return SpringTransactionProvider(txManager)
+    }
+
+    @Bean
     @Order(0)
-    fun jooqExceptionTranslatorExecuteListenerProvider(): DefaultExecuteListenerProvider {
-        return DefaultExecuteListenerProvider(JooqExceptionTranslator())
+    fun jooqExceptionTranslatorExecuteListenerProvider(
+        exceptionTranslatorExecuteListener: ExceptionTranslatorExecuteListener
+    ): DefaultExecuteListenerProvider {
+        return DefaultExecuteListenerProvider(exceptionTranslatorExecuteListener)
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(ExceptionTranslatorExecuteListener::class)
+    fun jooqExceptionTranslator(): ExceptionTranslatorExecuteListener {
+        return ExceptionTranslatorExecuteListener.DEFAULT
     }
 
     @Bean
